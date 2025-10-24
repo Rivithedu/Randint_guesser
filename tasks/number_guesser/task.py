781a3c46a0ec284cc380e6b729fed330bc027a29
Rivithuyangoda <157833630+Rivithuyangoda@users.noreply.github.com>
2025-10-23 23:47:25 -0700

    Initial Commit

--- tasks/number_guesser/task.py
+++ tasks/number_guesser/task.py
@@ -48,9 +48,9 @@ def get_name():
     return name
 def ordinal(n):
     if 11 <= (n % 100) <= 13:
-    suffix = 'th'
+        suffix = 'th'
     else:
-    suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
     return str(n) + suffix
 
 def show_past_games(player):
